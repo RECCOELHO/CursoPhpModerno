0b897553b7d755156d13df9a6e1ee725ffc4b879
Carlos Jessé Coelho <tv.coelho01@gmail.com>
2023-10-17 11:35:39 -0300

    update cad.php

--- Ex004/cad.php
+++ Ex004/cad.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -11,8 +12,11 @@
     </header>
     <main>
         <?php 
-            var_dump($_GET)
+            $name = $_GET["nome"] ?? "voce nao escreveu nem o nome";
+            $lastname = $_GET["sobrenome"] ?? " e nem sobrenome";
+            echo "<p> É um prazer te conhecer,<strong> $name $lastname! </strong>, este é o meu site";
         ?>
+        <p><a href="javascript:history.go(-1)">quer voltar para a pagina anterior? click aqui</a></p>
     </main>
 </body>
 </html>

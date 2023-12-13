b3445655fc46444ea6a3b23f20c4744c185fe48a
Carlos Jessé Coelho <tv.coelho01@gmail.com>
2023-12-13 11:43:20 -0300

    vam bora

--- desafios/processar.php
+++ desafios/processar.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,32 @@
     <title>Resiltado</title>
 </head>
 <body>
+    <main> 
+
+    <H1>Resultado Final</H1>
+    <?php
+            $numero = $_GET["numero"];
+            $anterior = $numero - 1;
+            $sucessor = $anterior + 2;
+
     
+    echo
+     "
+        <p>
+            O numero sugerido foi: <strong>$numero </strong>
+        </p>
+        <p>
+            O sucessor dele é: <strong> $sucessor </strong>
+        </p>
+        <p>
+            O antecessor dele é: <strong> $anterior </strong>
+        </p>
+    ";   
+    ?>
+    <br> <br>
+    <a class="botao" href="javascript:history.go(-1)">Voltar</a>
+
+    <strong></strong>
+    </main>
 </body>
 </html>

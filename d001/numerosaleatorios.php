0f4e49e9f39a06e2230bcfb38e90d6ff247d6d87
Carlos Jessé Coelho <63757384+RECCOELHO@users.noreply.github.com>
2025-03-21 15:58:20 -0300

    KEKAAA

--- d001/numerosaleatorios.php
+++ d001/numerosaleatorios.php
@@ -4,36 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Resiltado</title>
+    <title>Numeros Aleatorios</title>
 </head>
 <body>
-    <main> 
+    <main>
+        <h1>Rolagem de Dados pro RPG</h1>
+        <p>Gerando um número aleatorio entre 1 e 20...</p>
+        <p>Seu numero aleatorio é:
 
-    <H1>Resultado Final</H1>
-    <?php
-            $numero = $_GET["numero"];
-            $anterior = $numero - 1;
-            $sucessor = $anterior + 2;
-
-    
-    echo
-     "
-        <p>
-            O numero sugerido foi: <strong>$numero </strong>
+        <?php 
+            $numero = rand(1, 20);
+            echo $numero."<br>";
+            if($numero < 5 ){
+                echo "<p>Que azar.</p>";
+            } elseif($numero >= 6 && $numero <= 10 ){
+                echo "<p>Não é tão ruim, mas é ruim.</p>";
+            } elseif($numero >= 11 && $numero <= 15 ){
+                echo "<p>Deu Certo!</p>";
+            } elseif($numero >= 16 && $numero <= 20 ){
+                echo "<p>Critocooo!!! boa.</p>";
+            };
+        ?>
         </p>
-        <p>
-            O sucessor dele é: <strong> $sucessor </strong>
-        </p>
-        <p>
-            O antecessor dele é: <strong> $anterior </strong>
-        </p>
-    ";   
-    ?>
-    <br> <br>
-    <a class="botao" href="javascript:history.go(-1)">Voltar</a >
-
-    <strong></strong>
+        <button id="reloadButton">Recarregar Página</button>
     </main>
+    <script src="seu_script.js"></script>
 <!-- Code injected by live-server -->
 <script>
 	// <![CDATA[  <-- For SVG support
